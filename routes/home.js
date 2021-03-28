4ea33d89b59479b53e9660593a712fbebb9a401f
kylemcshea <kmcshea@stevens.edu>
2021-03-28 15:41:18 -0400

    identifying routes

--- routes/home.js
+++ routes/home.js
@@ -1,15 +1,14 @@
 // home route
 
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
+router.get("/", async (_, res) => {
+  try {
+    res.render("home", { layout: "main" });
+  } catch (e) {
+    res.status(404).json({ error: "Home page not found" });
+  }
+});
 
-router.get('/', async (_, res) => {
-    try {
-      res.render('home', {layout: 'main'});
-    } catch (e) {
-      res.status(404).json({ error: "Main page not found" })
-    }
-  })
-
-  module.exports = router
+module.exports = router;
