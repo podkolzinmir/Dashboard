da6a8c9167d9f6ebaa0ae1d4f13b3b13d17a9df1
kylemcshea <kmcshea@stevens.edu>
2021-03-29 21:22:36 -0400

    css issue fixed LFG

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
 // This is where we tie everything together
 
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const configRoutes = require('./routes');
-const exphbs = require('express-handlebars');
-const static = express.static(__dirname + '/public');
+const configRoutes = require("./routes");
+const exphbs = require("express-handlebars");
+const static = express.static(__dirname + "/public");
 
 app.use(express.json());
 
 configRoutes(app);
 
 const handlebarsInstance = exphbs.create({
-  defaultLayout: 'main',
+  defaultLayout: "main",
+  extname: ".handlebars",
   // Specify helpers which are only registered on this instance.
   helpers: {
     asJSON: (obj, spacing) => {
-      if (typeof spacing === 'number')
+      if (typeof spacing === "number")
         return new Handlebars.SafeString(JSON.stringify(obj, null, spacing));
 
       return new Handlebars.SafeString(JSON.stringify(obj));
-    }
-  }
+    },
+  },
 });
 
-app.engine('handlebars', handlebarsInstance.engine);
-app.set('view engine', 'handlebars');
-app.use('/public', static);
+app.engine("handlebars", handlebarsInstance.engine);
+app.set("view engine", "handlebars");
+app.use("/public", static);
 
 app.listen(3000, () => {
   console.log("We've now got a server!");
-  console.log('Your routes will be running on http://localhost:3000');
+  console.log("Your routes will be running on http://localhost:3000");
 });
