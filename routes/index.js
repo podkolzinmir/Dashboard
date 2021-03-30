9b9a9c35570fcfd1a4c1214dfea2ff426091f839
podkolzinmir <podkolzinmiriam@gmail.com>
2021-03-30 12:23:24 -0400

    some front end changes to main page

--- routes/index.js
+++ routes/index.js
@@ -12,9 +12,9 @@ const constructorMethod = (app) => {
     app.use('/', homeRoutes);
 
 
-    app.use('*', (req, res) => {
-      res.sendStatus(404);
-    });
+  //  app.use('*', (req, res) => {
+    //  res.sendStatus(404);
+    //});
   };
 
 module.exports = constructorMethod;
