0c4dd4ddbf57c7132475c3b34363e83366dea4ba
podkolzinmir <podkolzinmiriam@gmail.com>
2021-04-06 13:22:37 -0400

    graph added

--- routes/graph.js
+++ routes/graph.js
@@ -8,6 +8,8 @@ router.get("/", async (_, res) => {
   var data = fetchData();
   var failCount = 0;
   var passCount = 0;
+  var pSafe = 0;
+  var pUnsafe = 0;
   for (dp of data) {
     if (dp.models_passed == 0) {
       passCount++;
@@ -15,12 +17,21 @@ router.get("/", async (_, res) => {
     if (dp.models_passed == 3) {
       failCount++;
     }
+    if (dp.models_passed == 1){
+      pSafe++;
+    }
+    if (dp.models_passed == 2){
+      pUnsafe++;
+    }
   }
+
+  var barArr = [passCount, pSafe, pUnsafe, failCount];
   var pieArr = [passCount, failCount];
   console.log(pieArr);
+  console.log(barArr);
   try {
     console.log(data);
-    res.render("graph", { data, pieArr });
+    res.render("graph", { data, pieArr, barArr });
   } catch (e) {
     res.status(404).json({ error: "Graph page not found" });
   }
@@ -37,7 +48,7 @@ function fetchData() {
     //we have a safe situation "100%"
     //OR
     //we don't have a safe situation "100%"
-    if (dataPoint.models_passed == 0 || dataPoint.models_passed == 3) {
+    if (dataPoint.models_passed == 0 || dataPoint.models_passed == 3 ||dataPoint.models_passed == 2 || dataPoint.models_passed == 1 ) {
       returnArr.push(dataPoint);
     }
   }
